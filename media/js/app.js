94180cd25e6bbc458022bb06a3145d03243f67da
braintrain <abductee@gmail.com>
2014-07-31 02:51:37 -0400

    just some facebook integration login clickz

--- media/js/app.js
+++ media/js/app.js
@@ -1,4 +1,5 @@
-angular.module('facebookStats', [
-    'base',
-    'facebook'
-])
+angular
+    .module('facebookStats', [
+        'base',
+        'facebook'
+    ]);
--- media/js/facebook/facebook.js
+++ media/js/facebook/facebook.js
@@ -1,1 +1,5 @@
-angular.module('facebook', []);
+angular
+    .module('facebook', [])
+    .controller('FBController', ['$document', '$scope', function($document, $scope){
+
+    }]);
