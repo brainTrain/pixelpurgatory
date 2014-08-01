a06c3d1e244342b735a023aa50609b2b23a87235
braintrain <abductee@gmail.com>
2014-08-01 14:22:06 -0400

    got facebook logins workin on click, w00t!
    * imported and used ngFacebook from here!! \o/ https://github.com/GoDisco/ngFacebook

--- media/js/app.js
+++ media/js/app.js
@@ -1,5 +1,21 @@
 angular
     .module('facebookStats', [
         'base',
+        'ngFacebook',
         'facebook'
-    ]);
+    ])
+    .config(function($facebookProvider){
+        $facebookProvider.setAppId('1446543078942368');
+    })
+    .run(function($rootScope) {
+        (function() {
+            // only load if facebook isn't already loaded
+            if(!document.getElementById('facebook-jssdk')) {
+                var firstScriptElement = document.getElementsByTagName('script')[0], 
+                    facebookJS = document.createElement('script'); 
+
+                facebookJS.src = '//connect.facebook.net/en_us/all.js';
+                firstScriptElement.parentNode.insertBefore(facebookJS, firstScriptElement);
+            }
+        }());
+    });
--- media/js/facebook/facebook.js
+++ media/js/facebook/facebook.js
@@ -1,5 +1,23 @@
-angular
-    .module('facebook', [])
-    .controller('FBController', ['$document', '$scope', function($document, $scope){
-
+angular.module('facebook', [])
+    .controller('FBController', ['$facebook', '$scope', function($facebook, $scope){
+        $scope.isLoggedIn = false;
+        $scope.FBLogin =  function() {
+            $facebook
+                .login()
+                .then(function() {
+                    refresh();  
+                });
+        };
+        function refresh() {
+            $facebook
+                .api('/me')
+                .then(function(response) {
+                        $scope.yo = 'whatup ' + response.name + '?';
+                        $scope.isLoggedIn = true;
+                    },
+                    function(error) {
+                        $scope.yo = 'gotta log in dawg';
+                    });
+        }
+        refresh();
     }]);
