5a55f3c8095da9ed817a72ad65f14d052513f149
braintrain <abductee@gmail.com>
2014-08-04 22:09:13 -0400

    set up cache factory for storing facebook's return blobs
    * system to get four different status update types and update a cache to store them in
    * TODO: figure out how I can get these arrays to represent a specific static time range

--- media/js/facebook/controllers.js
+++ media/js/facebook/controllers.js
@@ -1,5 +1,5 @@
 angular.module('facebook.controllers', [])
-    .controller('facebookController', ['$facebook', '$scope', 'facebookAPIService', function($facebook, $scope, facebookAPIService) {
+    .controller('facebookController', ['$facebook', '$scope', 'facebookAPIService', 'facebookAPICache', function($facebook, $scope, facebookAPIService, facebookAPICache) {
         $scope.isLoggedIn = false;
         $scope.FBLogin =  function() {
             $facebook
@@ -15,28 +15,105 @@ angular.module('facebook.controllers', [])
                     $scope.yo = 'gotta log in dawg';
                     $scope.uglyFeed = '';
                     $scope.isLoggedIn = false;
+                })
+                .finally(function(){
+                    // clear out cache when the user logs out
+                    facebookAPICache.put('facebookAPIData', {})
                 });
         };
         $scope.getMyShit =  function() {
+            var facebookData = facebookAPICache.get('facebookAPIData'),
+                requestComplete = {
+                    "gotStatuses" : false,
+                    "gotPhotos" : false,
+                    "gotVideos" : false,
+                    "gotLinks" : false
+                };
+
+            // update the until value so we know when these requests were made
+            // and make sure that sucker's an int so I don't have to do conversions elsewhere
+            facebookData.data.until = parseInt(new Date().getTime()/1000, 10);
+
             facebookAPIService
-                    .getPostedStatuses()
-                    .then(function(response) {
-                            $scope.uglyFeed = response;
-                        },
-                        function(error) {
-                            $scope.uglyFeed = 'damn, errorz :\'(';
-                        });
+                .getPostedStatuses()
+                .then(function(success) {
+                        facebookData.data.statuses = success.data;
+                    }, function(error) {
+                        facebookData.data.statuses = [];
+                })
+                .finally(function() {
+                    requestComplete.gotStatuses = true;    
+                    updateCacheData(facebookData, requestComplete);
+                });
+            facebookAPIService
+                .getPostedPhotos()
+                .then(function(success) {
+                        facebookData.data.photos = success.data;
+                    }, function(error) {
+                        facebookData.data.photos = [];
+                })
+                .finally(function() {
+                    requestComplete.gotPhotos = true;
+                    updateCacheData(facebookData, requestComplete);
+                });
+            facebookAPIService
+                .getPostedVideos()
+                .then(function(success) {
+                        facebookData.data.videos = success.data;
+                    }, function(error) {
+                        facebookData.data.videos = [];
+                })
+                .finally(function() {
+                    requestComplete.gotVideos = true;
+                    updateCacheData(facebookData, requestComplete);
+                });
+            facebookAPIService
+                .getPostedLinks()
+                .then(function(success) {
+                        facebookData.data.links = success.data;
+                    }, function(error) {
+                        facebookData.links = [];
+                })
+                .finally(function() {
+                    requestComplete.gotLinks = true;
+                    updateCacheData(facebookData, requestComplete);
+                });
         };
         function logMeIn() {
             $facebook
                 .api('/me')
                 .then(function(response) {
+                        // set new cache on login
+                        var cachedResponse = {
+                            "user": {
+                                "id": response.id,
+                                "name": response.name,
+                                "first_name": response.first_name,
+                                "last_name": response.last_name
+                            },
+                            "data": {
+                                "until": "",
+                                "links": [],
+                                "statuses": [],
+                                "photos": []
+                            }
+                        };
+
                         $scope.yo = 'whatup ' + response.name + '?';
                         $scope.isLoggedIn = true;
+
+                        facebookAPICache.put('facebookAPIData', cachedResponse)
                     },
                     function(error) {
                         $scope.yo = 'gotta log in dawg';
                     });
-        }
+        };
+        function updateCacheData(facebookData, requestComplete) {
+            var allComplete = requestComplete.gotStatuses === true && requestComplete.gotPhotos === true && requestComplete.gotVideos === true && requestComplete.gotLinks === true ;
+            if(allComplete) {
+                facebookAPICache.put('facebookAPIData', facebookData);
+            }
+        };
+
         logMeIn();
     }]);
--- media/js/facebook/services.js
+++ media/js/facebook/services.js
@@ -4,20 +4,27 @@ angular.module('facebook.services', [])
             facebookPromise;
 
         facebookAPI.getPostedStatuses = function() {
-            facebookPromise = $facebook.api('/v2.0/me/posts');
-
+            facebookPromise = $facebook.api('/v2.0/me/statuses?limit=100');
             return facebookPromise;
         }; 
 
         facebookAPI.getPostedPhotos = function() {
-            facebookPromise = $facebook.api('/v2.0/me/photos/uploaded');
+            facebookPromise = $facebook.api('/v2.0/me/photos/uploaded?limit=100');
+            return facebookPromise;
+        }; 
+
+        facebookAPI.getPostedVideos = function() {
+            facebookPromise = $facebook.api('/v2.0/me/videos/uploaded?limit=100');
             return facebookPromise;
         }; 
 
         facebookAPI.getPostedLinks = function() {
-            facebookPromise = $facebook.api('/v2.0/me/links');
+            facebookPromise = $facebook.api('/v2.0/me/links?limit=100');
             return facebookPromise;
         }; 
 
         return facebookAPI;
+    })
+    .factory('facebookAPICache', function($cacheFactory) {
+        return $cacheFactory('facebookAPIData');
     });
